6a1a8f4aad49d82d14473a1ab971ccdcc8202ded
Leoxyzua <apinky549@gmail.com>
2021-11-26 02:14:01 -0500

    ;

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import {
     Constants,
     InteractionCommandClient,
     ShardClient
-} from 'detritus-client';
-import { GatewayIntents } from 'detritus-client-socket/lib/constants.js';
+} from 'detritus-client'
+import { GatewayIntents } from 'detritus-client-socket/lib/constants.js'
 import { inspect } from 'util'
 
-import './database';
-import './utils/detritus.esm';
+import './database'
+import './utils/detritus.esm'
 
-import handlers from './handlers';
-import handlePermissions from './utils/command.permissions';
+import handlers from './handlers'
+import handlePermissions from './utils/command.permissions'
 
-const { BOT_TOKEN } = process.env;
+const { BOT_TOKEN } = process.env
 
 const client = new ShardClient(BOT_TOKEN!, {
     gateway: {
@@ -39,28 +39,28 @@ const client = new ShardClient(BOT_TOKEN!, {
         // just 10 mins
         messages: { expire: 60 * 100 * 100 }
     }
-});
+})
 
-const interactionClient = new InteractionCommandClient(client);
+const interactionClient = new InteractionCommandClient(client)
 const messageClient = new CommandClient(client, {
     activateOnEdits: true,
     prefix: '-'
-});
+})
 
-handlers(client, interactionClient);
+handlers(client, interactionClient)
 
 try {
-    await client.run();
+    await client.run()
 
-    await interactionClient.addMultipleIn('src/commands/interactions');
-    await interactionClient.run();
+    await interactionClient.addMultipleIn('src/commands/interactions')
+    await interactionClient.run()
 
-    await handlePermissions(client);
+    await handlePermissions(client)
 
-    await messageClient.addMultipleIn('src/commands/message');
-    await messageClient.run();
+    await messageClient.addMultipleIn('src/commands/message')
+    await messageClient.run()
 } catch (error: any) {
-    console.error('Something went wrong with the clients', error.raw?.errors ? inspect(error.raw.errors, { depth: 7 }) : error);
+    console.error('Something went wrong with the clients', error.raw?.errors ? inspect(error.raw.errors, { depth: 7 }) : error)
 }
 
-console.log(`Logged in as ${client.user!.tag}, with ${interactionClient.commands.size} [/] commands and a shard count of ${client.shardCount} in ${client.guilds.size} guilds.`);
+console.log(`Logged in as ${client.user!.tag}, with ${interactionClient.commands.size} [/] commands and a shard count of ${client.shardCount} in ${client.guilds.size} guilds.`)
