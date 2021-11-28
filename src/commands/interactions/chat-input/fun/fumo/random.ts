10410960637f41e9fe6b690dbd5104b4f2366409
Leoxyzua <apinky549@gmail.com>
2021-11-28 01:09:47 -0500

    asi move

--- src/commands/interactions/chat-input/fun/fumo/random.ts
+++ src/commands/interactions/chat-input/fun/fumo/random.ts
@@ -1,17 +1,17 @@
-import { BaseSubCommand } from "../../../basecommand";
-import { Client } from ".";
-import { Constants, Interaction, Utils } from "detritus-client";
-import { Colors } from "../../../../../utils/constants";
-import { UpdateIcon } from "../../../../../utils/icons";
+import { BaseSubCommand } from "../../../basecommand"
+import { Client } from "."
+import { Constants, Interaction, Utils } from "detritus-client"
+import { Colors } from "../../../../../utils/constants"
+import { UpdateIcon } from "../../../../../utils/icons"
 
-export const COMMAND_NAME = "random";
+export const COMMAND_NAME = "random"
 
 export class FumoRandomCommand extends BaseSubCommand {
-    name = COMMAND_NAME;
-    description = "Obten un fumo aleatorio de la Fumo Api";
+    name = COMMAND_NAME
+    description = "Obten un fumo aleatorio de la Fumo Api"
 
     run(context: Interaction.InteractionContext | Utils.ComponentContext) {
-        const { URL, _id } = Client.cache.random;
+        const { URL, _id } = Client.cache.random
 
         const embed = new Utils.Embed()
             .setColor(Colors.INVISIBLE)
