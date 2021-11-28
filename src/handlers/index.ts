6f9788705b1947dfcc5349e8caeb9ea133d69f6b
Leoxyzua <apinky549@gmail.com>
2021-11-28 18:39:00 -0500

    Use `subscribe` instead of `on`

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -7,10 +7,10 @@ import { GuildBanAdd, ThreadUpdate } from "./gateway"
 process.on('unhandledRejection', console.log)
 
 export default function (client: ShardClient, commandClient: InteractionCommandClient) {
-    commandClient.on(ClientEvents.COMMAND_RAN, CommandRan)
+    commandClient.subscribe(ClientEvents.COMMAND_RAN, CommandRan)
 
-    client.on(ClientEvents.GUILD_BAN_ADD, GuildBanAdd)
-    client.on(ClientEvents.THREAD_UPDATE, ThreadUpdate)
+    client.subscribe(ClientEvents.GUILD_BAN_ADD, GuildBanAdd)
+    client.subscribe(ClientEvents.THREAD_UPDATE, ThreadUpdate)
 
     console.log('Handlers up')
 }
